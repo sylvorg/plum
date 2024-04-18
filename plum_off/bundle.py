903254c5e535558866fd8c319307890c609a56b7
sylvorg <jeet.ray@syvl.org>
2024-04-18 17:29:46 +0000

    init2

--- plum_off/bundle.py
+++ plum_off/bundle.py
@@ -3,7 +3,7 @@ from .python import plumeta, process
 
 class Bundle(plumeta, metaclass=plumeta):
     def __new__(cls, name, bases, ns):
-        return super().__new__(cls, name, bases, ns)
+        pass
 
 
 class bundle(metaclass=Bundle):
